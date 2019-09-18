648f9ceac161ce76b0434a743e158ec8b2882b11
Antanas Kompanas <a.kompanas@gofintech.lt>
2019-09-18 16:03:08 +0300

    INSERT helper.

--- src/FDO.php
+++ src/FDO.php
@@ -53,4 +53,58 @@ class FDO
             throw new PDOException("FDO::query() statement prepare failed");
         return new FDOQuery($statement);
     }
+
+    /**
+     * Helper for building INSERT INTO statements.
+     * 
+     * @param string $table target table name
+     * @param array $values field values ['field_name' => $value]
+     * @param array|null $options additional feature control
+     * 
+     * @example
+     *  $fdo->insert('order', ['customer' => 7], [
+     *      'returning' => ['id']
+     *  ]);
+     * 
+     * @return FDOQuery
+     */
+    public function insert(string $table, array $values, ?array $options = null): FDOQuery
+    {
+        $sql = ['insert into ' . self::quoteIdentifier($table) . ' ('];
+        $params = [];
+        $bindGroup = [];
+        $i = 0;
+        foreach ($values as $name => $value) {
+            if ($i > 0) {
+                $sql[] = ',';
+                $bindGroup[] = ',';
+            }
+            $sql[] = self::quoteIdentifier($name);
+            $paramName = ":p$i";
+            $params[$paramName] = $value;
+            $bindGroup[] = $paramName;
+            $i++;
+        }
+        $sql[] = ') values (' . implode($bindGroup) . ')';
+        if (isset($options['returning'])) {
+            $sql[] = ' returning ';
+            $first = true;
+            foreach ($options['returning'] as $field) {
+                if (!$first) {
+                    $sql[] = ',';
+                }
+                $sql[] = self::quoteIdentifier($field);
+                $first = false;
+            }
+        }
+        return $this->query(implode($sql))->params($params);
+    }
+    
+    public static function quoteIdentifier(string $name): string 
+    {
+        if (preg_match('/^[a-zA-Z][a-zA-Z0-9_]*$/', $name))
+            return $name;
+        else
+            return '"' . str_replace('"', '""', $name) . '"';
+    }
 }
